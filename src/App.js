d6e1845f290f462f4ad59d007792573de338b44b
Seonho <dkdlatjsh@gmail.com>
2021-10-23 12:12:19 +0900

    fix : axios 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = (props) => {
+  useEffect(() => {
+    const apiCall = async () => {
+      // let username = "hoonkk";
+      const response = await axios.get(
+        `https://46344938-b432-4f5a-bdfe-2aaf77f751ee.mock.pstmn.io/trips`
+      );
+      console.log(response.data);
+    };
+    apiCall();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"./logo192.png"} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +31,8 @@ function App() {
       </header>
     </div>
   );
-}
+
+  // function App() {
+};
 
 export default App;
