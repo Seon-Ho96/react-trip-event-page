56fe94747d527a5d1956a39cb1afe86e9ba81726
Seonho <dkdlatjsh@gmail.com>
2021-10-31 18:27:32 +0900

    App.js 라우터 설정

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 
+import { Route, Switch } from "react-router";
+import { Home, Tap1, Tap2 } from "../pages";
+// import { Menu } from "../components";
+
 class App extends Component {
   render() {
-    return <div>Hello React-Router</div>;
+    return (
+      <div>
+        <Route exact path="/" component={Home} />
+        <Switch>
+          {/* <Route path="/about/:name" component={About} /> */}
+          <Route path="/tap1" component={Tap1} />
+          <Route path="/tap2" component={Tap2} />
+        </Switch>
+      </div>
+    );
   }
 }
 
